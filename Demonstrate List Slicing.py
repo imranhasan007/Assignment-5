443441dd8d6c858f2510492e5681145b295559c1
imranhasan007 <imranhasanansari0786@gmail.com>
2025-12-31 16:33:05 +0530

    Initial Commit

---
+++ Demonstrate List Slicing.py
@@ -0,0 +1,11 @@
+
+numbers = list(range(1, 11))
+
+
+first_five_elements = numbers[:5]
+
+extracted_elements = first_five_elements[::-1]
+
+print("Original list:", numbers)
+print("Extracted first five elements:", first_five_elements)
+print("Reversed extracted elements:", extracted_elements)
--- Read a File and Handle Errors.py
+++
@@ -1,8 +0,0 @@
-filename = 'sample.txt'
-try:
-    with open("sample.txt", 'rt') as file:
-        print("Reading file content:")
-        for idx, line in enumerate(file, start=1):
-            print(f"Line {idx}: {line.strip()}")
-except FileNotFoundError:
-    print(f"Error: The file '{filename}' was not found.")
